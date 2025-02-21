f792ffad6a93c7458580cdd7a92a9593ed877a75
vincent560 <vincent69021@gmail.com>
2025-02-21 14:13:53 +0800

    P3 P4 P5

--- demo/w01_dom_36/js/w01_36.js
+++ demo/w01_dom_36/js/w01_36.js
@@ -1,5 +1,9 @@
 const userInput = document.querySelector('.input-number');
 const addBtn = document.querySelector('.btn-add');
+const subtractBtn = document.querySelector('.btn-subtract');
+const multiplyBtn = document.querySelector('.btn-multiply');
+const divideBtn = document.querySelector('.btn-divide');
+
 
 const currentResultOutput = document.querySelector('.current-result');
 const currentCalculationOutput = document.querySelector('.current-calculation');
@@ -15,14 +19,58 @@ function getUserInput(){
   return parseInt(userInput.value);
 }
 
+function outputResult(result, text){
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent =text;
+}
+
 function add(){
-  const operand1 =currentResult;
-  const operand2 =getUserInput();
+  const operand1 = parseFloat(currentResult);
+  const operand2 = getUserInput();
   currentResult = operand1 + operand2;
-  currentResultOutput.textContent = currentResult;
+  //currentResultOutput.textContent = currentResult;
   const calText = `${operand1} + ${operand2}`;
-  console.log('calText: ', calText);
-  currentCalculationOutput.textContent =calText;
+  console.log(`${calText} = ${currentResult}`);
+  //currentCalculationOutput.textContent =calText;
+  outputResult(currentResult, calText)
 }
 
 addBtn.addEventListener('click', add);
+
+function subtract() {
+  const operand1 = parseFloat(currentResult);
+  const operand2 = getUserInput();
+  currentResult = operand1 - operand2;
+  const calText = `${operand1} - ${operand2}`;
+  console.log(`${calText} = ${currentResult}`);
+  outputResult(currentResult, calText);
+}
+
+subtractBtn.addEventListener('click', subtract);
+
+function multiply() {
+  const operand1 = parseFloat(currentResult);
+  const operand2 = getUserInput();
+  currentResult = operand1 * operand2;
+  const calText = `${operand1} * ${operand2}`;
+  console.log(`${calText} = ${currentResult}`);
+  outputResult(currentResult, calText);
+}
+
+multiplyBtn.addEventListener('click', multiply);
+
+function divide(){
+  const operand1 =parseFloat(currentResult);
+  const operand2 =getUserInput();
+  if (operand2 === 0)
+    alert('cannot divide by 0')
+  else {
+  currentResult = (operand1 / operand2).toFixed(2);
+  // currentResultOutput.textContent = currentResult;
+  const calText = `${operand1} / ${operand2}`;
+  console.log(`${calText} = ${currentResult}`);
+  // currentCalculationOutput.textContent =calText;
+  outputResult(currentResult, calText);
+  }
+}
+divideBtn.addEventListener('click', divide);
