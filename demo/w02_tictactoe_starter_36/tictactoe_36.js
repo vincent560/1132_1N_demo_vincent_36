bdc6c72e855ad87a640c0ddf8a2c8c4778a1fa90
vincent560 <vincent69021@gmail.com>
2025-02-28 23:56:48 +0800

    p2 p3

--- demo/w02_tictactoe_starter_36/tictactoe_36.js
+++ demo/w02_tictactoe_starter_36/tictactoe_36.js
@@ -7,3 +7,78 @@ const container = document.querySelector('.container');
 const alert = document.querySelector('.alert');
 const allLi = document.querySelectorAll('.board li');
 const resetBtn = document.querySelector('.reset');
+
+console.log(alert)
+console.log(allLi)
+
+const winMessage = (player) => {
+  if(player === 'o'){
+    container.style.backgroundColor = 'rgba(144,238,144,0.5)';
+    alert.style.backgroundColor = 'rgba(144,238,144,0.5)';
+    alert.style.color ='green';
+    alert.style.display ='block';
+    alert.textContent = 'player o wins';
+  } else {
+    container.style.
+    backgroundColor = 'rgba(240,118,128,0.726)';
+    alert.style.backgroundColor = 'rgba(240,118,128,0.726)';
+    alert.style.color ='purple';
+    alert.style.display ='block';
+    alert.textContent = 'player x wins';
+
+  }
+
+};
+
+const tieMessage = () => {
+  alert.style.backgroundColor ='#888';
+  alert.style.color = '#ddd'
+  alert.style.display ='block'
+  alert.textContent = 'tie'
+}
+
+tieMessage();
+
+const reset = () => {
+  alert.style.display = 'none'
+  container.style.backgroundColor ='#666'
+  allLi.forEach((item) => {
+    item.textContent ='+'
+    item.classList = '';
+
+  })
+}
+resetBtn.addEventListener('click', reset);
+
+winMessage('o');
+//winMessage('x');
+
+/*o   <li class="o">o</li>
+      <li class="o">o</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="x">x</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+*/
+/*x   <li class="x">x</li>
+      <li class="x">x</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+      <li class="o">o</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+*//*    <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+      <li class="x">x</li>
+      <li class="o">o</li>
+*/
